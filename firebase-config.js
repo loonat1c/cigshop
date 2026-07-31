2684c642421d22727a1e82023eca2c92ad82a646
loonat1c <serjjoga@gmail.com>
2026-07-31 16:05:35 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Вставь сюда конфиг своего Firebase-проекта
 // (Firebase Console → Project settings → General → "Your apps" → Web app → SDK setup and configuration)
 const firebaseConfig = {
-  apiKey: "ВСТАВЬ_СЮДА",
-  authDomain: "ВСТАВЬ_СЮДА.firebaseapp.com",
-  projectId: "ВСТАВЬ_СЮДА",
-  storageBucket: "ВСТАВЬ_СЮДА.appspot.com",
-  messagingSenderId: "ВСТАВЬ_СЮДА",
-  appId: "ВСТАВЬ_СЮДА"
+  apiKey: "AIzaSyDzxYXJnKp-M9kPL1j_DFIc1ivZrtKxhEI",
+  authDomain: "cigmagtash.firebaseapp.com",
+  projectId: "cigmagtash",
+  storageBucket: "cigmagtash.firebasestorage.app",
+  messagingSenderId: "575737299892",
+  appId: "1:575737299892:web:d7eb780b7106db54e828e1"
 };
